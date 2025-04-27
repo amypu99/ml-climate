5dfd10c45bc2a4f0c329976b8fa1a1243b8bf566
amypu99 <ap4489@columbia.edu>
2025-04-27 12:36:48 -0400

    add final results from first batch and also new reports olmocr

--- src/preprocess_climate_reports.py
+++ src/preprocess_climate_reports.py
@@ -26,6 +26,22 @@ def create_indices_round_2_reports():
             with open(f"climate_reports/round_2_reports_olmocr_indexed/"+filename, "w") as outfile:
                 outfile.write(json.dumps(data))
 
+def count_training_data():
+    training_examples = set()
+    for year in ["2022", "2023", "2024"]:
+        document_folder = f"climate_reports/ccrm_{year}_olmocr/indexed/"
+        for filename in os.listdir(document_folder):
+            training_examples.add(filename.replace(document_folder, "").replace(".jsonl", ""))
+        document_folder = f"climate_reports/round_2_reports_olmocr_indexed/{year}/"
+        for filename in os.listdir(document_folder):
+            training_examples.add(filename.replace(document_folder, "").replace(".jsonl", ""))
+    document_folder = f"climate_reports/tp_reports/"
+    for filename in os.listdir(document_folder):
+        training_examples.add(filename.replace(document_folder, "").replace(".pdf", ""))
+    print(training_examples)
+    print(len(training_examples))
+
 if __name__ == "__main__":
     # create_indices_ccrm()
-    create_indices_round_2_reports()
+    # create_indices_round_2_reports()
+    count_training_data()
--- src/run_olmcr.sh
+++ src/run_olmcr.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Loop over every .pdf file in cases_temp/DNMS
-for file in climate_reports/round_3_reports/unilever_2020.pdf
+for file in climate_reports/tp_reports/*.pdf
 do
   # Run the Python pipeline command on the current file
-  python3 -m olmocr.pipeline climate_reports/round_3_reports_olmocr/unilever_2020.jsonl --pdfs climate_reports/round_3_reports/unilever_2020.pdf
+  python3 -m olmocr.pipeline climate_reports/tp_reports_olmocr --pdfs "$file" --workers=6
 done
